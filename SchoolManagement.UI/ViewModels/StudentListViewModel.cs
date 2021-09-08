e087c003924ed9abcbe8b50cdfb4c6fd20331448
nbourre <nbourrereg@hotmail.com>
2021-09-08 11:46:11 -0400

    Moved some files and added DataGrid to view

---
+++ SchoolManagement.UI/ViewModels/StudentListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using SchoolManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SchoolManagement.UI.ViewModels
+{
+    public class StudentListViewModel : BaseViewModel
+    {
+        StudentDataService data = new StudentDataService();
+
+        ObservableCollection<Student> students;
+
+        public ObservableCollection<Student> Students
+        {
+            get => students;
+            set
+            {
+                students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public StudentListViewModel()
+        {
+            Students = new ObservableCollection<Student>(data.GetAll());
+        }
+
+    }
+}
--- SchoolManagement.UI/Views/MainWindow.xaml.cs
+++ SchoolManagement.UI/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolManagement.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,13 @@ namespace SchoolManagement.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        StudentListViewModel vm = new StudentListViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = vm;
         }
     }
 }
