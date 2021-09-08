8b6812df58766e7c998eff402fa2525f46c787d7
nbourre <nbourrereg@hotmail.com>
2021-09-08 11:32:33 -0400

    Add project files.

---
+++ SchoolManagement.Models/IDataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagement.Models
+{
+    interface IDataService<T>
+    {
+        public IEnumerable<T> GetAll();
+    }
+}
